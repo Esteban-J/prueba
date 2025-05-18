2e4a2fd4712449d434c1604e5719d73fde992251
Gerardo <a273880@uach.mx>
2025-05-18 17:10:57 -0600

    Contador real-time con Node.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+let contador = 0;
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+  console.log('Cliente conectado');
+
+  socket.emit('contador', contador);
+
+  socket.on('incrementar', () => {
+    contador++;
+    io.emit('contador', contador);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
+  });
+});
+
+//const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor iniciado en http://localhost:${PORT}`);
+});
