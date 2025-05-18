6adec0d992b91e19fe69dda932ecec25efb279df
Gerardo <a273880@uach.mx>
2025-05-18 17:24:32 -0600

    dfdf

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ io.on('connection', (socket) => {
   });
 });
 
-//const PORT = 3000;
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Servidor iniciado en http://localhost:${PORT}`);
